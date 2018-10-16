ac0ed647ddcfb04e3a1f1db4cc18ed4d69d7e652
ak158428101 <158428101@qq.com>
2018-10-16 17:18:15 +0800

    BUG FIX
    Change-Id: I9c9e96217f7165ff1d1d682be86e96fb7918bd88
    Signed-off-by: ak158428101 <158428101@qq.com>

---
+++ src/testJava/NetRequestAction.java
@@ -0,0 +1,77 @@
+package testJava;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class NetRequestAction {
+	
+	
+	/**
+	 * 股海明灯上面的涨停板数据
+	 */
+	public static void zhangtingban() {
+		ArrayList<ZTBBean> list = new ArrayList<>();
+        
+        for(int pageNo = 1; pageNo <= 1000 ; pageNo++) {
+            String url = "http://www.178448.com/fjzt-2.html?page="+pageNo;
+            try {
+                Document doc = Jsoup.connect(url).get();
+                Elements elements =  doc.select("div.datalist").select("tbody").select("tr");
+                for(Element element : elements){
+                	ZTBBean bean = new ZTBBean(element.select("td").eachText());
+                	list.add(bean);
+                    System.out.println("xxyy "+bean);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("xxyy "+list);
+	}
+	
+
+	
+	/**
+	 * 股海明灯涨停板数据对象
+	 *
+	 */
+	static class ZTBBean {
+		String id;
+		String bzt;
+		String cgl;
+		String stockName;
+		String reson;
+		String date;
+		String selectPrice;
+		String bztNum;
+		String bztDate;
+		
+		public ZTBBean(List<String> list) {
+			id = list.get(0);
+			bzt= list.get(1);
+			cgl= list.get(2);
+			stockName= list.get(3);
+			reson= list.get(4);
+			date= list.get(5);
+			selectPrice= list.get(6);
+			bztNum= list.get(7);
+			bztDate= list.get(8);
+		}
+
+		@Override
+		public String toString() {
+			return "ZTBBean [id=" + id + ", bzt=" + bzt + ", cgl=" + cgl + ", stockName=" + stockName + ", reson="
+					+ reson + ", date=" + date + ", selectPrice=" + selectPrice + ", bztNum=" + bztNum + ", bztDate="
+					+ bztDate + "]";
+		}
+		
+		
+	}
+}
--- src/testJava/SQLiteJDBC.java
+++ src/testJava/SQLiteJDBC.java
@@ -1,17 +1,22 @@
 package testJava;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-
-import testJava.Main.Bean;
+import java.util.Date;
 
 public class SQLiteJDBC {
+
+	public static String dataPath = "D:\\new_tdx\\code\\data\\date";
 	
 	public static void createTable() {
 
@@ -25,8 +30,20 @@ public class SQLiteJDBC {
 	      statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
 //	      statement.executeUpdate("drop table if exists data");
-	      statement.executeUpdate("create table if not exists data (id text, date text, open double, high double, low double, close double, amount double,turnover double,constraint pk_t2 primary key (id,date)) ");
-		  statement.executeUpdate("create table if not exists setting (name primary key,value text)");
+	      statement.executeUpdate("create table if not exists data ("
+	      		+ "id text,"
+	      		+ " date text, "
+	      		+ "open double, "
+	      		+ "high double, "
+	      		+ "low double, "
+	      		+ "close double, "
+	      		+ "amount double,"
+	      		+ "turnover double,"
+	      		+ "constraint pk_t2 primary key (id,date)) ");
+	      
+		  statement.executeUpdate("create table if not exists setting ("
+		  		+ "name primary key,"
+		  		+ "value text)");
 		    
 		  long dateUpdateTime = -1;
 	      ResultSet rs = statement.executeQuery("select * from setting");
@@ -38,14 +55,21 @@ public class SQLiteJDBC {
 	    		  dateUpdateTime = Long.valueOf(value); 
 	    	  }
 	      }
+	      
+
+			SimpleDateFormat sDateFormat=new SimpleDateFormat("yyyy/MM/dd");
+			Date d = new Date();
+			d.setTime(dateUpdateTime);
+			String date = sDateFormat.format(d);
+			statement.execute("delete from data where date like '"+date+"'");
 		  
 	      ArrayList<Bean> list = new ArrayList<>();
 	      PreparedStatement prep = connection.prepareStatement(
 	    	      "insert into data values (?,?,?,?,?,?,?,?);");
 	      
-		    File file = new File(Main.dataPath);
+		    File file = new File(dataPath);
 		    for(File subFile : file.listFiles()) {
-				list = dateUpdateTime > 0 ? Main.readFileByLines(subFile.getAbsolutePath(),dateUpdateTime):Main.readFileByLines(subFile.getAbsolutePath());
+				list = dateUpdateTime > 0 ? readFileByLines(subFile.getAbsolutePath(),dateUpdateTime):readFileByLines(subFile.getAbsolutePath());
 				for(Bean bean : list){
 //				      statement.executeUpdate("insert into data values("+subFile.getName().substring(3, 9)+", '',"+bean.date+","+bean.open+","+bean.high+","+bean.low+","+bean.close+","+bean.amount+","+bean.turnover+")");
 					String name = subFile.getName().substring(3, 9);
@@ -58,7 +82,7 @@ public class SQLiteJDBC {
 					prep.setDouble(7, bean.amount); 
 					prep.setDouble(8, bean.turnover); 
 		    	    prep.addBatch();
-					System.out.println("insert "+subFile.getName());
+//					System.out.println("insert "+subFile.getName());
 				}
 		    }
 		    
@@ -83,6 +107,7 @@ public class SQLiteJDBC {
 	    }
 	    catch(Exception e)
 	    {
+	    	System.err.println(e.getMessage());
 	      // if the error message is "out of memory", 
 	      // it probably means no database file is found
 	    	try {
@@ -91,7 +116,6 @@ public class SQLiteJDBC {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-	    	System.err.println(e.getMessage());
 	    }
 	    finally
 	    {
@@ -157,4 +181,107 @@ public class SQLiteJDBC {
 		return list;
 	}
 
+
+	/**
+	 * 以行为单位读取文件，常用于读面向行的格式化文件
+	 */
+	public static ArrayList<Bean> readFileByLines(String fileName) {
+		ArrayList<Bean> list = new ArrayList<>();
+	    File file = new File(fileName);
+	    BufferedReader reader = null;
+	    try {
+	        reader = new BufferedReader(new FileReader(file));
+	        String tempString = null;
+	        int line = 1;
+	        // 一次读入一行，直到读入null为文件结束
+	        while ((tempString = reader.readLine()) != null ) {
+	        	String[] array = tempString.split(",");
+	        	if(array.length == 7) {
+//		            System.out.println("line " + line + ": " + tempString);
+	        		Bean bean = new Bean(array);
+	        		list.add(bean);
+	        	}
+	            line++;
+	        }
+	        reader.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (reader != null) {
+	            try {
+	                reader.close();
+	            } catch (IOException e1) {
+	            }
+	        }
+	    }
+	    return list;
+	}
+	
+	public static ArrayList<Bean> readFileByLines(String fileName,long time) {
+		SimpleDateFormat sDateFormat=new SimpleDateFormat("yyyy/MM/dd");
+		Date d = new Date();
+		d.setTime(time);
+		String date = sDateFormat.format(d);
+		
+		ArrayList<Bean> list = new ArrayList<>();
+	    File file = new File(fileName);
+	    BufferedReader reader = null;
+	    try {
+	        reader = new BufferedReader(new FileReader(file));
+	        String tempString = null;
+	        int line = 1;
+	        // 一次读入一行，直到读入null为文件结束
+	        while ((tempString = reader.readLine()) != null ) {
+	        	String[] array = tempString.split(",");
+	        	if(array.length == 7) {
+//		            System.out.println("line " + line + ": " + tempString);
+	        		Bean bean = new Bean(array);
+	        		if(bean.date.compareTo(date) >= 0) {
+		        		list.add(bean);
+	        		}
+	        	}
+	            line++;
+	        }
+	        reader.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (reader != null) {
+	            try {
+	                reader.close();
+	            } catch (IOException e1) {
+	            }
+	        }
+	    }
+	    return list;
+	}
+	
+	static class Bean {
+		String id;
+		String date;
+		Double open;
+		Double high;
+		Double low;
+		Double close;
+		Double amount;
+		Double turnover;
+		
+		Bean(){}
+		
+		Bean(String[] array) {
+			date = array[0];
+			open = Double.valueOf(array[1]);
+			high = Double.valueOf(array[2]);
+			low = Double.valueOf(array[3]);
+			close = Double.valueOf(array[4]);
+			amount = Double.valueOf(array[5]);
+			turnover = Double.valueOf(array[6]);
+		}
+
+		@Override
+		public String toString() {
+			return "Bean [id=" + id + ", date=" + date + ", open=" + open + ", high=" + high + ", low=" + low
+					+ ", close=" + close + ", amount=" + amount + ", turnover=" + turnover + "]";
+		}
+	}
 }
